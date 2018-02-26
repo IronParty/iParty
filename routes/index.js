5c8c9af36aeab323411aff383ebe3a312ec0b4bc
Enrique Garcia <rickyg218@gmail.com>
2018-02-26 10:29:46 -0600

    this is my first commit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,82 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const salt = bcrypt.genSaltSync(10);
+const passport   = require('passport');
+const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
 
-/* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+
+router.get("/signup", (req,res, next)=>{
+    res.render("auth/signup");
+})
+
+.post("/signup", (req,res,next)=>{
+    const username = req.body.username,
+          password = req.body.password;
+    if(username === "" || password === ""){
+        res.render("auth/signup", {message: "Please enter username and password"});
+        return;
+    }
+
+    User.findOne({username}, "username", (err, user)=>{
+       if (user !== null){
+           res.render("auth/signup", {message:"The username already exists, please enter a different username"});
+           return;
+       }
+
+       const hashPass = bcrypt.hashSync(password, salt);
+
+       const newUser = new User({
+          username,
+          password:hashPass
+       });
+
+       newUser.save(err=>{
+           if (err) return res.render("auth/signup", { message: "Something went wrong" });
+            res.redirect("/");
+       });
+
+    });
 });
 
+
+
+router.get('/login', ensureLoggedOut(), (req, res) => {
+    res.render('authentication/login', { message: req.flash('error')});
+});
+
+router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
+  successRedirect : '/',
+  failureRedirect : '/login',
+  failureFlash : true
+}));
+
+router.get('/signup', ensureLoggedOut(), (req, res) => {
+    res.render('authentication/signup', { message: req.flash('error')});
+});
+
+router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
+  successRedirect : '/',
+  failureRedirect : '/signup',
+  failureFlash : true
+}));
+
+router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
+    res.render('authentication/profile', {
+        user : req.user
+    });
+});
+
+router.post('/logout', ensureLoggedIn('/login'), (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+
 module.exports = router;
+
+
+
+
+
