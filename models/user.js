8b72fbf0199e89478dae4a568127eafea380df55
AliciaJz <zilaphono@gmail.com>
2018-02-27 16:57:55 -0600

    lalala

--- models/user.js
+++ models/user.js
@@ -5,6 +5,9 @@ const userSchema = new Schema({
   userName   : {type: String ,trim:true },
   password   : {type: String },
   email: {type: String},
+  googleID:{type: String},
+  facebookID: {type: String}
+  
 });
 
 const User = mongoose.model('User', userSchema);
