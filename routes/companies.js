1d8ca786dfc577bcd384601ddd87b5a7f26a4d2e
AliciaJz <zilaphono@gmail.com>
2018-02-28 11:07:19 -0600

    antes de cambios de miercoles

--- routes/companies.js
+++ routes/companies.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const multer = require("multer")
+
 router.get('/', function(req, res, next) {
-  
   res.render('companies/companies',{companies:null});
 });
 
