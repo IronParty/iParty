dcff0bb1961185b7419ac6f06c6ecb70e5b2b0d4
AliciaJz <zilaphono@gmail.com>
2018-02-28 17:13:08 -0600

    cambios bliss

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,26 @@
 const express = require("express");
 const router = express.Router();
 const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const User = require("../models/User");
 
 router.get("/profile", ensureLoggedIn("/login"), (req, res) => {
-    res.render("auth/profile", {
+    res.render("user/edit", {
       user: req.user
     });
   });
+
+  router.post("/profile", (req,res)=>{
+    User.findOneAndUpdate({_id:req.user._id}, {
+      username:req.body.name,
+      email:req.body.email
+    })
+    .then(result=>{
+      res.redirect("/user/profile");
+    })
+    .catch(err=>res.send("error"+err));
+  });
+
+
 
   // document.getElementById('edit-user-form').onsubmit = function(e){
   //   e.preventDefault();
