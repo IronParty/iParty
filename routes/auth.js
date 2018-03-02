b2b3756166b15ac0826367e980e455b50e84f0f2
AliciaJz <zilaphono@gmail.com>
2018-03-01 23:38:54 -0600

    delete picture model

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User.js");
+const User = require("../models/user.js");
 const bcrypt = require("bcrypt");
 const salt = bcrypt.genSaltSync(10);
 const passport   = require('passport');
